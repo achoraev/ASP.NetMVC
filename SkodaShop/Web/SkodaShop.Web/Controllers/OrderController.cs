9d7fbb5d58133a3f84fa51bb0d18a255119b5273
achoraev <audulin@gmail.com>
2014-11-17 01:23:46 +0200

    create partial order view

--- SkodaShop/Web/SkodaShop.Web/Controllers/OrderController.cs
+++ SkodaShop/Web/SkodaShop.Web/Controllers/OrderController.cs
@@ -1,13 +1,23 @@
 ﻿namespace SkodaShop.Web.Controllers
 {
+    using SkodaShop.Data.Common.Repository;
+    using SkodaShop.Data.Models;
     using System.Web.Mvc;
 
     public class OrderController : Controller
     {
-        // GET: Order
+        private IRepository<Order> orders;
+
+        public OrderController(IRepository<Order> orders)
+        {
+            this.orders = orders;
+        }
+
         public ActionResult Index()
         {
-            return this.View();
+            var orders = this.orders.All();
+
+            return this.View(orders);
         }
     }
 }
